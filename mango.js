ca9d0cdfacbbf6646c7b52378575b7c1ac802949
SHREYASI (cosmetic aurora) <74972696+shreyasinegi@users.noreply.github.com>
2021-09-08 10:12:05 +0530

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,26 @@
+class Mango{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution :0,
+            friction :1,
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/mango.png")
+		this.body=Bodies.circle(this.x, this.y, this.r, options)
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var mangoPos=this.body.position;	
+		push();
+		translate(mangoPos.x, mangoPos.y);
+		imageMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		pop();
+ }
+}
---
+++ tree.js
@@ -0,0 +1,29 @@
+class Tree
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.treeWidth=450;
+		this.treeHeight=600;
+		this.treeThickness=10;
+		
+		this.image=loadImage("images/tree.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.treeWidth, this.treeThickness, {isStatic:true})
+		World.add(world, this.bottomBody)
+
+	}
+	
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.treeHeight/2,this.treeWidth, this.treeHeight)
+			pop()
+			
+	}
+
+}
